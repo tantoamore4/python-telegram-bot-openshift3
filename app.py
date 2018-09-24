2caf1ccc06f8a425a5103a1d9da10049eccf2c2b
crucian84 <crucian84@users.noreply.github.com>
2018-09-24 07:30:36 +0600

    Change Telegram bot token

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Updater, Fi
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-TOKEN = 'YOUR-TELEGRAM-BOT-TOKEN'
+TOKEN = '693266929:AAHv5cYEQmTI0kkClSWNK-CtRr7oqrXp3mI'
 
 
 def start(bot, update):
