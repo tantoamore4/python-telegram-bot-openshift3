c33a3aefb3d58b190086807879a7dcd2036a63e5
crucian84 <ukraine270@gmail.com>
2018-10-15 17:12:06 +0600

    add comments

--- app.py
+++ app.py
@@ -40,18 +40,19 @@ def button(bot, update):
 def ar(bot, update):
     random_num = random.randint(1,100)
     guess_num = True
-    num = update.message.text
+    #num = int(update.message.text)
     try:
-        #while guess_num == True:
-        if random_num == int(num):
-            ar = 'You win!'
-            guess_num = False
-        elif random_num > int(num):
-            ar = 'Your number less than mine. Try one more time :)'
-        elif random_num < int(num):
-            ar = 'Your number bigger than mine. Try one more time :)'
-        else:
-            ar = 'Foo'
+        while guess_num == True:
+            num = int(update.message.text)
+            if random_num == num:
+                ar = 'You win!'
+                guess_num = False
+            elif random_num > num:
+                ar = 'Your number less than mine. Try one more time :)'
+            elif random_num < num:
+                ar = 'Your number bigger than mine. Try one more time :)'
+            else:
+                ar = 'Foo'
         #ar = int(num) * 370
     except (NameError, SyntaxError):
         ar = "Error"
