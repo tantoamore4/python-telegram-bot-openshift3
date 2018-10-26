16aca0f0f87489dd88afa77ea8ffc717d73ab606
crucian84 <ukraine270@gmail.com>
2018-10-26 13:08:23 +0600

    add comments

--- app.py
+++ app.py
@@ -48,13 +48,11 @@ def ar(bot, update):
             if random_num == num:
                 ar = 'Вы выиграли!'
                 num_true = False
-                bot.send_message(chat_id=update.message.chat_id, text=ar)
             elif random_num > num:
                 ar = 'Ваше число меньше моего. Попробуйте еще раз :)'
-                bot.send_message(chat_id=update.message.chat_id, text=ar)
             elif random_num < num:
                 ar = 'Ваше число больше моего. Попробуйте еще раз :)'
-                bot.send_message(chat_id=update.message.chat_id, text=ar)
+            bot.send_message(chat_id=update.message.chat_id, text=ar)
         # ar = int(num) * 370
     except (NameError, SyntaxError, ValueError):
         ar = "Введите целое число"
